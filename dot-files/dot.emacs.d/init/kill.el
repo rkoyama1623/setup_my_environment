b0e81365a731946e2b3775ef6f0906f63f3fa0e1
Ryo Koyama <ryo.koyama@outlook.jp>
2020-05-09 15:46:54 +0900

    [dot.emacs.d/init/kill.el] enable to sync clipboard in mac

--- dot-files/dot.emacs.d/init/kill.el
+++ dot-files/dot.emacs.d/init/kill.el
@@ -1,11 +1,27 @@
-;;clip board
-(setq interprogram-paste-function
-      (lambda ()
-         (shell-command-to-string "xsel -b -o")))
+;; Sync Clip Board
+;; linux
+(defun copy-from-linux (text &optional rest)
+  (let* ((process-connection-type nil)
+         (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
+    (process-send-string proc text)
+    (process-send-eof proc)))
+(defun paste-to-linux ()
+  (shell-command-to-string "xsel -b -o"))
+;; os x
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
 
-(setq interprogram-cut-function
-      (lambda (text &optional rest)
-        (let* ((process-connection-type nil)
-               (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
-          (process-send-string proc text)
-          (process-send-eof proc)))))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+;; switch based on os
+(if (eq system-type 'darwin)
+    (progn
+      (setq interprogram-cut-function 'paste-to-osx)
+      (setq interprogram-paste-function 'copy-from-osx))
+  (progn
+    (setq interprogram-cut-function 'paste-to-linux)
+    (setq interprogram-paste-function 'copy-from-linux)))
