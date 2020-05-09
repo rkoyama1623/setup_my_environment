17a3ad6d4e7faca6de75becdce9c316a3c1902cc
Ryo Koyama <ryo.koyama@outlook.jp>
2020-05-09 16:21:25 +0900

    [dot.emacs.d::apearance.el]  whitespace color settings

--- dot-files/dot.emacs.d/init/appearance.el
+++ dot-files/dot.emacs.d/init/appearance.el
@@ -1,2 +1,30 @@
 ;; scroll step
 (setq scroll-conservatively 1)
+
+;; whitespace
+(require 'whitespace)
+;; 空白
+(set-face-foreground 'whitespace-space nil)
+(set-face-background 'whitespace-space "red")
+;; ファイル先頭と末尾の空行
+(set-face-background 'whitespace-empty "yellow")
+;; タブ
+(set-face-foreground 'whitespace-tab nil)
+(set-face-background 'whitespace-tab "yellow")
+;; 空白文字を強制表示
+(set-face-background 'whitespace-trailing "pink")
+(set-face-background 'whitespace-hspace "pink")
+(setq whitespace-style '(face           ; faceで可視化
+                         trailing       ; 行末
+                         tabs           ; タブ
+                         empty          ; 先頭/末尾の空行
+                         spaces         ; 空白
+                         ;; space-mark     ; 表示のマッピング
+                         tab-mark))
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+;; タブの表示を変更
+(setq whitespace-display-mappings
+      '((tab-mark ?\t [?\xBB ?\t])))
+;; 発動
+(global-whitespace-mode 1)
