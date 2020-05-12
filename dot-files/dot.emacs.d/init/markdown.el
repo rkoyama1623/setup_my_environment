47f39e70304315b307eaf61b3bafb92baae89dc5
Ryo Koyama <ryo.koyama@outlook.jp>
2020-05-13 01:10:31 +0900

    fix bug in markdown-mode.el

--- dot-files/dot.emacs.d/init/markdown.el
+++ dot-files/dot.emacs.d/init/markdown.el
@@ -1,5 +1,8 @@
-(defun open-with-shiba ()
-  "open a current markdown file with shiba"
-  (interactive)
-  (start-process "shiba" "*shiba*" "shiba" "--detach" buffer-file-name))
-(define-key markdown-mode-map (kbd "C-c C-c") 'open-with-shiba)
+(add-hook 'markdown-mode-hook
+	(lambda ()
+	  (defun open-with-shiba ()
+	    "open a current markdown file with shiba"
+	    (interactive)
+	    (start-process "shiba" "*shiba*" "shiba" "--detach" buffer-file-name))
+	  (define-key markdown-mode-map (kbd "C-c C-c") 'open-with-shiba)))
+
