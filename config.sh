bdff3f3ea35599cab0985e780008b3e59d2fefec
Ryo Koyama <ryo.koyama@outlook.jp>
2020-07-11 13:43:35 +0900

    install pyenv

--- config.sh
+++ config.sh
@@ -96,6 +96,20 @@ if [ ${ret} -eq 0 ]; then
     ./ntp.sh
     echo "done";fi
 
+# pyenv
+yes_or_no_while "set pyenv. Do you agree? (yes/no)";ret=$?;
+if [ ${ret} -eq 0 ]; then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    pyenv init -
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
+    echo "done";fi
+
 
 
 # firefox
