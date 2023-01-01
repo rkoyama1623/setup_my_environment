369628333aa078a56bf54c91a25241b10eeb03e5
Ryo Koyama <ryo.koyama@outlook.jp>
2023-01-01 13:41:22 +0900

    Add setup script for mac

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,2 +1,13 @@
 # use .bashrc in mac
-echo -e 'if [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi' >> ~/.bash_profile 
+# echo -e 'if [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi' >> ~/.bash_profile
+sudo pmset -a disablesleep 1 
+brew install tmux
+brew install git tig
+
+brew install libffi xz zlib sqlite3 pyenv pyenv-virtualenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+
+
