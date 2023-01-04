ee53668c584a75e9eaa2f5fba3b2b62164f058ef
Ryo Koyama <ryo.koyama@outlook.jp>
2023-01-04 18:03:19 +0900

    Add cmake

--- setup_mac.sh
+++ setup_mac.sh
@@ -4,7 +4,7 @@ sudo pmset -a disablesleep 1
 brew install tmux
 brew install git tig
 
-brew install libffi xz zlib sqlite3 pyenv pyenv-virtualenv
+brew install libffi cmake xz zlib sqlite3 pyenv pyenv-virtualenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
