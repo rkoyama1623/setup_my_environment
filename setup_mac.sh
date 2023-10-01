75b704afbc0e06ac8c8a1dc42c98d6280326e18b
Ryo Koyama <ryo.koyama@outlook.jp>
2023-10-01 18:57:08 +0900

    update for setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -2,7 +2,7 @@
 # echo -e 'if [ -f ~/.bashrc ]; then\n    source ~/.bashrc\nfi' >> ~/.bash_profile
 sudo pmset -a disablesleep 1 
 brew install tmux
-brew install git tig
+brew install git tig python-tk
 
 brew install libffi cmake xz zlib sqlite3 pyenv pyenv-virtualenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
@@ -10,4 +10,10 @@ echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
 
+brew install libjpeg # For pillow
+echo 'export PATH="/usr/local/opt/jpeg/bin:$PATH"' >> ~/.zshrc
+echo 'export LDFLAGS="-L/usr/local/opt/jpeg/lib"' >> ~/.zshrc
+echo 'export CPPFLAGS="-I/usr/local/opt/jpeg/include"' ~/.zshrc
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/jpeg/lib/pkgconfig"' >> ~/.zshrc
 
+echo 'backend: TkAgg' >> ~/.matplotlib/matplotlibrc
